47469731515870fada5476037534321389ec22bc
Grant Phelps <gbphelps2@gmail.com>
2018-06-20 09:34:34 -0400

    Use setState callbacks to clean up date-setting logic

--- CalendarFrontEnd/components/time.jsx
+++ CalendarFrontEnd/components/time.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 
 
 
+function format(num){
+  if (num < 10) return '0' + num;
+  return num;
+}
+
+
 export default class Time extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      
+      hour: this.hour(),
+      minute: this.props.date.getMinutes(),
+      suffix: this.suffix()
     }
-  }
-
-  minute(){
-    const m = this.props.date.getMinutes();
-    if (m < 10) return '0' + m;
-    return m;
   }
 
   hour(){
     const fullHour = this.props.date.getHours();
     if ( fullHour === 0) return 12;
     const h = (fullHour > 12 ? fullHour - 12 : fullHour);
-    if (h < 10) return '0' + h;
     return h;
   }
 
@@ -29,60 +30,66 @@ export default class Time extends React.Component{
     return ( h >= 12 ? 'pm' : 'am' )
   }
 
-  setHours(e){
-    const current = +e.currentTarget.value;
-    const h = this.props.date.getHours();
+
+  updateTime(){
+    let hour = this.state.hour % 12;
+    if (this.state.suffix === 'pm') hour += 12;
     const date = new Date(this.props.date.valueOf());
-
-    if (current > 1){e.currentTarget.nextElementSibling.select();}
-    if (Number.isNaN(current) || current > 12) return;
-
-    const newhours = current % 12 + (h >= 12 ? 12 : 0);
-    date.setHours(newhours);
+    date.setHours(hour,this.state.minute);
     this.props.set(date);
+  }
+
+  setHours(e){
+    const target = e.currentTarget;
+    const current = +target.value;
+    if (Number.isNaN(current) || current > 12) return;
+    this.setState({hour: current},()=>{
+      if (current > 1) target.nextElementSibling.select();
+    });
   }
 
   setMins(e){
-    const current = +e.currentTarget.value;
-    const date = new Date(this.props.date.valueOf());
-
-    if (current > 5){e.currentTarget.nextElementSibling.select();}
+    const target = e.currentTarget;
+    const current = +target.value;
     if (Number.isNaN(current) || current > 60) return;
-    date.setMinutes(current);
-    this.props.set(date);
+    this.setState({minute: current},() => {
+      if (current > 5) target.nextElementSibling.select();
+    });
   }
 
   setSuffix(e){
-    const h = this.props.date.getHours();
-    const current = e.currentTarget.value;
-    const date = new Date(this.props.date.valueOf());
+    const target = e.currentTarget;
+    const current = target.value;
 
-    if (current === 'p' && h < 12){
-      date.setHours(h + 12);
-      this.props.set(date);
-    }
-    if (current === 'a' && h >= 12){
-      date.setHours(h - 12);
-      this.props.set(date);
-    }
-    e.currentTarget.blur();
+    let suffix;
+    if (current === 'a'){suffix = 'am';}
+    else if (current === 'p'){suffix = 'pm';}
+    else {return;}
+
+    this.setState({ suffix }, ()=>{
+      target.blur();
+    })
   }
 
   render(){
+    console.log(this.props.date);
     return (
       <div style={{display:'flex', alignItems:'center'}}>
         <input
           onClick={e => e.currentTarget.select()}
           onChange={e=>this.setHours(e)}
-          value={this.hour()}/>:
+          onBlur={()=>this.updateTime()}
+          value={format(this.state.hour)}/>:
         <input
           onClick={e => e.currentTarget.select()}
           onChange={e=>this.setMins(e)}
-          value={this.minute()}/>
+          onBlur={()=>this.updateTime()}
+          value={format(this.state.minute)}/>
         <input
           onClick={e => e.currentTarget.select()}
           onChange={e=>this.setSuffix(e)}
-          value={this.suffix()}/>
+          onBlur={()=>this.updateTime()}
+          value={this.state.suffix}/>
       </div>
     );
   }
